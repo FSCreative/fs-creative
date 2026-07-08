b0e0cf7984b5f6a08dfc27e69694937dbe5869d5
FSCreative <simon@fs-creative.at>
2026-07-08 17:29:41 +0200

    Mobile: App-Shell (Tab-Leiste, Bottom-Sheet, PWA)

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ const TYPES = {
   ".woff2": "font/woff2",
   ".txt": "text/plain; charset=utf-8",
   ".xml": "application/xml; charset=utf-8",
+  ".webmanifest": "application/manifest+json; charset=utf-8",
 };
 
 // ---------------------------------------------------------------------------
